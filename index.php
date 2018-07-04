4941825d099471018486f7b7ddfc43fd3677e4cb
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-04 14:20:47 +0700

    up

--- index.php
+++ index.php
@@ -3,32 +3,31 @@
  * PROCESSANDO A MENSAGEM 
  * QUE CHEGA DO BOT
  */
-echo '555';
-// function processMessage($update) {
-//     if($update["result"]["action"] == "buscar.nfe"){
-//         sendMessage(array(
-//             "source" => $update["result"]["source"],
-//             "speech" => "..........TEXT HERE...........",
-//             "displayText" => ".........TEXT HERE...........",
-//             "contextOut" => array()
-//         ));
-//     }
-// }
-// /*
-//  * FUNÇÃO PARA ENVIAR A MENSAGEM
-//  */
-// function sendMessage($parameters) {
-//     echo json_encode($parameters);
-// }
+function processMessage($update) {
+    if($update["result"]["action"] == "buscar.nfe"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "..........TEXT HERE...........",
+            "displayText" => ".........TEXT HERE...........",
+            "contextOut" => array()
+        ));
+    }
+}
+/*
+ * FUNÇÃO PARA ENVIAR A MENSAGEM
+ */
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
 
-// /*
-//  * PEGANDO A REQUISIÇÃO
-//  */
-// $update_response = file_get_contents("php://input");
-// $update = json_decode($update_response, true);
-
-// if (isset($update["result"]["action"])) {
-//     processMessage($update);
-// }
+/*
+ * PEGANDO A REQUISIÇÃO
+ */
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+echo $update;
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
 
 ?>
