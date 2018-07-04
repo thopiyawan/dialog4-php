7406f56c6308188ef6acbf2a48a5cc8c5911f627
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-04 15:06:54 +0700

    up

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
  * PROCESSANDO A MENSAGEM 
  * QUE CHEGA DO BOT
  */
-function processMessage($update) {
-    if($update["result"]["action"] == "buscar.nfe"){
-        print_r('55');
+function processMessage($text) {
+    if($text == "แพ้อาหาร"){
+   
         sendMessage(array(
             "source" => $update["result"]["source"],
             "speech" => "..........TEXT HERE...........",
@@ -14,6 +14,8 @@ function processMessage($update) {
         ));
     }
 }
+
+
 /*
  * FUNÇÃO PARA ENVIAR A MENSAGEM
  */
@@ -27,11 +29,21 @@ function sendMessage($parameters) {
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 
+ if(!is_null($update)){
+            // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+            $replyToken  = $events['events'][0]['replyToken'];
+            $user = $events['events'][0]['source']['userId'];
+            $text = $events['events'][0]['message']['text'];
+            $type_message = $events['events'][0]['message']['type'];
+            processMessage($text);
 
-if (isset($update["result"]["action"])) {
+ }
+             
 
-      processMessage($update);
+// if (isset($update["result"]["action"])) {
 
-}
+//       processMessage($update);
+
+// }
 
 ?>
