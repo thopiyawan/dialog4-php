4209d26ccecf24c2597b61fc015c53bb8f83fea9
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-04 11:29:47 +0700

    r

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * PROCESSANDO A MENSAGEM 
+ * QUE CHEGA DO BOT
+ */
+function processMessage($update) {
+    if($update["result"]["action"] == "buscar.nfe"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "..........TEXT HERE...........",
+            "displayText" => ".........TEXT HERE...........",
+            "contextOut" => array()
+        ));
+    }
+}
+/*
+ * FUNÇÃO PARA ENVIAR A MENSAGEM
+ */
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+/*
+ * PEGANDO A REQUISIÇÃO
+ */
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
+
+?>
