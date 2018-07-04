9e01b1f1edf8b644dbb38fc6c97605327986a3a1
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-04 16:00:18 +0700

    up

--- index.php
+++ index.php
@@ -1,49 +1,22 @@
 <?php
-/*
- * PROCESSANDO A MENSAGEM 
- * QUE CHEGA DO BOT
- */
-function processMessage($text) {
-    if($text == "แพ้อาหาร"){
-   
-        sendMessage(array(
-            "source" => "agent",
-            "speech" => "..........TEXT HERE...........",
-            "displayText" => ".........TEXT HERE...........",
-            "contextOut" => array()
-        ));
+
+
+require 'vendor/autoload.php';
+
+use Google\Cloud\Dialogflow\V2\EntityTypesClient;
+
+$entityTypesClient = new EntityTypesClient();
+$projectId = '[MY_PROJECT_ID]';
+$entityTypeId = '[ENTITY_TYPE_ID]';
+$formattedEntityTypeName = $entityTypesClient->entityTypeName($projectId, $entityTypeId);
+
+$entityType = $entityTypesClient->getEntityType($formattedEntityTypeName);
+foreach ($entityType->getEntities() as $entity) {
+    print(PHP_EOL);
+    printf('Entity value: %s' . PHP_EOL, $entity->getValue());
+    print('Synonyms: ');
+    foreach ($entity->getSynonyms() as $synonym) {
+        print($synonym . "\t");
     }
+    print(PHP_EOL);
 }
-
-
-/*
- * FUNÇÃO PARA ENVIAR A MENSAGEM
- */
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
-
-/*
- * PEGANDO A REQUISIÇÃO
- */
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-
- if(!is_null($update)){
-            // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-            // $replyToken  = $update ['events'][0]['replyToken'];
-            // $user = $update['events'][0]['source']['userId'];
-            $text = $update['events'][0]['message']['text'];
-            // $type_message = $update['events'][0]['message']['type'];
-            processMessage($text);
-
- }
-             
-
-// if (isset($update["result"]["action"])) {
-
-//       processMessage($update);
-
-// }
-
-?>
