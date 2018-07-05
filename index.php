4225335e47f3d93930198139d54edd2fa9cf3b01
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-05 16:03:56 +0700

    up

--- index.php
+++ index.php
@@ -4,28 +4,16 @@
 function processMessage($update) {
     if($update["result"]["action"] == "sayHello"){
         sendMessage(array(
-             "speech"=> "",
-                      "messages"=> [array(
-                                       "title"=> 'Oblivion',
-                                       "subtitle"=> 'Oblivion is a SciFi film.',
-                                       "buttons"=> [ array(
-                                                      "text"=> "view film",
-                                                      "postback"=>"https://www.moovrika.com/m/3520"
-                                                  ),
-                                           array(
-                                                      "text"=> "Ask something else",
-                                                      "postback"=>"I want to ask something else"
-                                                  )
-                                            ]
-                                          ,
-                                        "type"=> 1
-                                   )]
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
         ));
     }
 }
 
 function sendMessage($parameters) {
-	header('Content-Type: application/json');
+   header('Content-Type: application/json');
     echo json_encode($parameters, JSON_UNESCAPED_SLASHES);
 }
 
