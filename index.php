df0b977cbcf51e709d195c1139be26d2adf0be48
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-05 15:39:12 +0700

    f

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 
 
-require 'vendor/autoload.php';
-
-use Google\Cloud\Dialogflow\V2\EntityTypesClient;
-
-$entityTypesClient = new EntityTypesClient();
-$projectId = '[MY_PROJECT_ID]';
-$entityTypeId = '[ENTITY_TYPE_ID]';
-$formattedEntityTypeName = $entityTypesClient->entityTypeName($projectId, $entityTypeId);
-
-$entityType = $entityTypesClient->getEntityType($formattedEntityTypeName);
-foreach ($entityType->getEntities() as $entity) {
-    print(PHP_EOL);
-    printf('Entity value: %s' . PHP_EOL, $entity->getValue());
-    print('Synonyms: ');
-    foreach ($entity->getSynonyms() as $synonym) {
-        print($synonym . "\t");
+function processMessage($update) {
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
     }
-    print(PHP_EOL);
+}
+
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
 }
