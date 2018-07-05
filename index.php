bfb715dba32c337115200c6a31e1aee60e10f1d1
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-05 16:32:25 +0700

    up

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 
 
-function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
-        ));
-    }
-}
+// function processMessage($update) {
+//     if($update["result"]["action"] == "sayHello"){
+//         sendMessage(array(
+//             "source" => $update["result"]["source"],
+//             "speech" => "Hello from webhook",
+//             "displayText" => "Hello from webhook",
+//             "contextOut" => array()
+//         ));
+//     }
+// }
 
-function sendMessage($parameters) {
-   header('Content-Type: application/json');
-    echo json_encode($parameters, JSON_UNESCAPED_SLASHES);
-}
+// function sendMessage($parameters) {
+//    header('Content-Type: application/json');
+//     echo json_encode($parameters, JSON_UNESCAPED_SLASHES);
+// }
 
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
+// $update_response = file_get_contents("php://input");
+// $update = json_decode($update_response, true);
+// if (isset($update["result"]["action"])) {
+//     processMessage($update);
+// }
