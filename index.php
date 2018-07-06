1d30b1a8841a639d3d9b406d855c8b74ad659339
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-06 11:48:55 +0700

    h

--- index.php
+++ index.php
@@ -38,14 +38,14 @@ function detect_intent_texts($projectId, $text, $sessionId, $languageCode = 'th'
     $displayName = $intent->getDisplayName();
     $confidence = $queryResult->getIntentDetectionConfidence();
     $fulfilmentText = $queryResult->getFulfillmentText();
- 
+  print($getFulfillmentText);
     // output relevant info
-    print(str_repeat("=", 20) . PHP_EOL);
-    printf('Query text: %s' . PHP_EOL, $queryText);
-    printf('Detected intent: %s (confidence: %f)' . PHP_EOL, $displayName,
-        $confidence);
-    print(PHP_EOL);
-    printf('Fulfilment text: %s' . PHP_EOL, $fulfilmentText);
+    // print(str_repeat("=", 20) . PHP_EOL);
+    // printf('Query text: %s' . PHP_EOL, $queryText);
+    // printf('Detected intent: %s (confidence: %f)' . PHP_EOL, $displayName,
+    //     $confidence);
+    // print(PHP_EOL);
+    // printf('Fulfilment text: %s' . PHP_EOL, $fulfilmentText);
  
     $sessionsClient->close();
 }
