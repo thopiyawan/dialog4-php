4e15fc391d135d64ce648f3506ee6e0973a38717
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-07-06 11:52:06 +0700

    p

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ function detect_intent_texts($projectId, $text, $sessionId, $languageCode = 'th'
     $displayName = $intent->getDisplayName();
     $confidence = $queryResult->getIntentDetectionConfidence();
     $fulfilmentText = $queryResult->getFulfillmentText();
-  print($getFulfillmentText);
+    echo $getFulfillmentText;
     // output relevant info
     // print(str_repeat("=", 20) . PHP_EOL);
     // printf('Query text: %s' . PHP_EOL, $queryText);
